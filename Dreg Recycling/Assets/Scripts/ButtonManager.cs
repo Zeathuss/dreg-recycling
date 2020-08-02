b3a2fa773fc30aed9d7d38b5d899932d1c100e57
Zeathuss <leggosmax@gmail.com>
2020-08-02 21:05:39 +0200

    inventory be backs

--- Dreg Recycling/Assets/Scripts/ButtonManager.cs
+++ Dreg Recycling/Assets/Scripts/ButtonManager.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
     public GameObject sellingPanel;
+    public GameObject inventoryPanel;
+    public Button inventoryButton;
+    public Sprite[] backpackSprites;
 
     public void closeSelling()
     {
         sellingPanel.SetActive(false);
+    }
+
+    public void openInventory()
+    {
+        inventoryPanel.SetActive(true);
+        inventoryButton.image.sprite = backpackSprites[1];
+    }
+    public void closeInventory()
+    {
+        inventoryPanel.SetActive(false);
+        inventoryButton.image.sprite = backpackSprites[0];
     }
 }

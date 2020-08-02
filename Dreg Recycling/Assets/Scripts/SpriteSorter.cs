d9df41f7afcc40fa2eb81f5f1515f800bf2cfbf7
DebeliGolub <69087904+DebeliGolub@users.noreply.github.com>
2020-08-02 14:06:31 +0200

    alah

---
+++ Dreg Recycling/Assets/Scripts/SpriteSorter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSorter : MonoBehaviour
+{
+    [SerializeField] private int sortingOrder = 5000;
+    [SerializeField] private int offset = 0;
+    [SerializeField] private bool runOnce = false;
+    private Renderer renderer;
+
+    private void Awake()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+    }
+    private void LateUpdate()
+    {
+        renderer.sortingOrder = (int)(sortingOrder - transform.position.y - offset);
+    }
+}

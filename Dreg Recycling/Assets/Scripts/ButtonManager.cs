d82c224bb09aef31e4f138a8aa41c7abc9041515
Zeathuss <leggosmax@gmail.com>
2020-08-02 20:16:27 +0200

    fuk u

--- Dreg Recycling/Assets/Scripts/ButtonManager.cs
+++ Dreg Recycling/Assets/Scripts/ButtonManager.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
     public GameObject sellingPanel;
+    [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private Button inventoryOpen;
+    [SerializeField] private Sprite[] backpackSprites;
 
     public void closeSelling()
     {
         sellingPanel.SetActive(false);
+    }
+
+    public void openInventory()
+    {
+        inventoryPanel.SetActive(true);
+        inventoryOpen.image.sprite = backpackSprites[1];
+    }
+
+    public void closedInventory()
+    {
+        inventoryPanel.SetActive(false);
+        inventoryOpen.image.sprite = backpackSprites[0];
     }
 }

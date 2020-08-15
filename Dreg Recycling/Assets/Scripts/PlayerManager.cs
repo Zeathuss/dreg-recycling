27b0f8536428766b76db7cc17afc626205a4100b
DebeliGolub <69087904+DebeliGolub@users.noreply.github.com>
2020-08-15 14:28:23 +0200

    vvsvv

--- Dreg Recycling/Assets/Scripts/PlayerManager.cs
+++ Dreg Recycling/Assets/Scripts/PlayerManager.cs
@@ -25,6 +25,14 @@ public class PlayerManager : MonoBehaviour
     public float moveWhileHungry;
     public float moveWhileThirsty;
 
+    [SerializeField] private float hungerSpeed;
+    [SerializeField] private float maxHungerSpeed = 1;
+    [SerializeField] private float minHungerSpeed = 30;
+    [SerializeField] private float hungerAmount = 1;
+    [SerializeField] private float thirstSpeed;
+    [SerializeField] private float maxThirstSpeed = 1;
+    [SerializeField] private float minThirstSpeed = 20;
+    [SerializeField] private float thirstAmount = 1;
 
     private void Start()
     {
@@ -32,9 +40,21 @@ public class PlayerManager : MonoBehaviour
         moveWhileThirsty = defaultSpeed / 2;
 
         moveSpeed = defaultSpeed;
+
+        hungerSpeed = minHungerSpeed;
+        thirstSpeed = minThirstSpeed;
+
+        StartCoroutine(HungerSpeed());
+        StartCoroutine(ThirstSpeed());
     }
     private void Update()
     {
+        if (hungerSpeed <= maxHungerSpeed) hungerSpeed = maxHungerSpeed;
+        if (thirstSpeed <= maxThirstSpeed) thirstSpeed = maxThirstSpeed;
+
+        if (hungerSpeed >= minHungerSpeed) hungerSpeed = minHungerSpeed;
+        if (thirstSpeed >= minThirstSpeed) thirstSpeed = minThirstSpeed;
+
         if (Hunger >= maxHunger)
         {
             moveSpeed = moveWhileHungry;
@@ -56,5 +76,44 @@ public class PlayerManager : MonoBehaviour
         {
             Time.timeScale = 1;
         }
+    }
+
+    IEnumerator HungerSpeed()
+    {
+        yield return new WaitForSeconds(hungerSpeed);
+
+        hungerAmount++;
+        hungerSpeed--;
+
+        StartCoroutine(HungerSpeed());
+    }
+
+    IEnumerator HungerAmount()
+    {
+        yield return new WaitForSeconds(hungerSpeed);
+
+        GameObject.Find("Player").GetComponent<PlayerManager>().Hunger += hungerAmount;
+
+        StartCoroutine(HungerAmount());
+    }
+
+
+    IEnumerator ThirstSpeed()
+    {
+        yield return new WaitForSeconds(thirstSpeed);
+
+        thirstAmount++;
+        thirstSpeed--;
+
+        StartCoroutine(ThirstSpeed());
+    }
+
+    IEnumerator ThirstAmount()
+    {
+        yield return new WaitForSeconds(thirstSpeed);
+
+        GameObject.Find("Player").GetComponent<PlayerManager>().Thirst += thirstAmount;
+
+        StartCoroutine(ThirstAmount());
     }
 }

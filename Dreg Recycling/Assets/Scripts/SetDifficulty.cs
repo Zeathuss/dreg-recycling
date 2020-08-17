7347bd6b3908af10383d48c61d55a6c82b6181e5
DebeliGolub <69087904+DebeliGolub@users.noreply.github.com>
2020-08-17 19:44:14 +0200

    fak ju

---
+++ Dreg Recycling/Assets/Scripts/SetDifficulty.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SetDifficulty : MonoBehaviour
+{
+    public string difficulty;
+    public Text difficultyText;
+    public GameObject difficultyTextObj;
+    public GameObject playButton;
+    
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(GameObject.Find("SetDifficulty"));
+    }
+
+    public void setDifficulty(string _difficulty)
+    {
+        difficulty = _difficulty;
+        playButton.SetActive(true);
+        difficultyTextObj.SetActive(true);
+
+    }
+
+    private void Update()
+    {
+        if (difficulty == "Easy")
+        {
+            difficultyText.text = "jakov je rušan";
+        }
+
+        if (difficulty == "Medium")
+        {
+            difficultyText.text = "jakov je fuj";
+        }
+
+        if (difficulty == "Hard")
+        {
+            difficultyText.text = "fk ju jakov";
+        }
+    }
+
+}

ecf9c8186f5bfd4689d1d030d5a3bb918bfcceb0
DebeliGolub <69087904+DebeliGolub@users.noreply.github.com>
2020-08-20 14:49:16 +0200

    đob

---
+++ Dreg Recycling/Assets/Scripts/MowingGrass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MowingGrass : MonoBehaviour
+{
+    public GameObject[] Grass;
+
+    private void Start()
+    {
+         
+    }
+}
